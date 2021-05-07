731e49ae962409af016ce75a53e67e2208003764
mkhoirurrizqi <muhammad.118140134@student.itera.ac.id>
2021-05-07 23:46:47 +0700

    update delete product feature

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -136,8 +136,8 @@ class ProductsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
-    {
-     //
-    }
+    // public function destroy($id)
+    // {
+    //  //
+    // }
 }
